81985e99af2b9c56867ec36b62861842b3233e9d
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2018-01-09 17:39:50 -0200

    Form parcial criado. Criação de controller feita

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,8 @@
 Rails.application.routes.draw do
-  get 'invoices/new'
-
-  get 'operations/new'
-
-  get 'operations/show'
-
-  resources :sellers, only: :new
-  resources :payers, only: :new
+  resources :sellers, only: [:new, :create]
+  resources :payers, only: [:new, :create]
   resources :operations, only: [:new, :show]
   resources :invoices, only: :new
-
-
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
