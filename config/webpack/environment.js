72c383a2a7c9ec1414707b2ca69b51f4c38bf6d0
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2018-01-23 15:02:08 -0200

    jquery intalado pelo yarn, bootstrap instalada pelo yarn, modal funcionando

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,4 +1,12 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = require('webpack')
+environment.plugins.set('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery'
+  })
+)
+
 module.exports = environment
 
